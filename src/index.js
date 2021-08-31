9aab23d22bc0a4e6015491e9db53632831d5bc4d
rapzaDev <rapzadev@gmail.com>
2021-08-31 15:54:30 -0300

    Challenge 03 - First Commit

--- src/index.js
+++ src/index.js
@@ -23,12 +23,12 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
   repositoryIndex = repositories.findindex(repository => repository.id === id);
 
@@ -36,9 +36,13 @@ app.put("/repositories/:id", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  Object.assign( repositories[repositoryIndex], {
+    title,
+    url,
+    techs
+  })
 
-  repositories[repositoryIndex] = repository;
+  const repository = repositories[repositoryIndex];
 
   return response.json(repository);
 });
@@ -48,11 +52,13 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  repositories.splice(repositoryIndex, 1);
+  const deletedRepository = repositories[repositoryIndex];
+
+  repositories.splice(deletedRepository, 1);
 
   return response.status(204).send();
 });
@@ -66,9 +72,11 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  const likeUpdatedRepository = repositories[repositoryIndex];
+
+  return response.json(likeUpdatedRepository);
 });
 
 module.exports = app;
